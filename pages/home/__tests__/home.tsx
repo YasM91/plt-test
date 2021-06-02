6a6aded7a6ed6c1ebc44eff126aec4a6166dfa26
Yaseen <yaseenm990@gmail.com>
2021-06-02 15:05:38 +0100

    test(home): tested home view

--- pages/home/__tests__/home.tsx
+++ pages/home/__tests__/home.tsx
@@ -1,8 +1,11 @@
-import {render} from '@testing-library/react';
+import {render, waitFor, screen} from '@testing-library/react';
 import Home from "../home";
-import userEvent from '@testing-library/user-event'
+import userEvent from '@testing-library/user-event';
+import {getStock} from "../../../functions/get-data"
 import "@testing-library/jest-dom/extend-expect";
 
+
+const stockSpy = jest.fn();
 
 describe('testing home component', () => {
     it('checks if dropdown component has values from json file as options', () =>{
@@ -16,5 +19,29 @@ describe('testing home component', () => {
         
         expect(select).toHaveValue('LTV719449/39/39')
     });
+    it('select value and find sku total', async () =>{
+        const {getByRole} = render(<Home/>);
+        
+        const select = getByRole('combobox', {name: /sku/i});
+        
+        userEvent.selectOptions(select, ["LTV719449/39/39"]);
+        
+        expect(select).toHaveValue('LTV719449/39/39');
 
+        await waitFor(() => {
+            expect(screen.queryByText(/remaining stock:/i)).toBeInTheDocument();
+        })
+    });
+    it('shows error message if no SKU in both stocks and transactions when choosing blank value', async () =>{
+        const {getByRole} = render(<Home/>);
+        
+        const select = getByRole('combobox', {name: /sku/i});
+        
+        userEvent.selectOptions(select, [""]);
+                
+        await waitFor(() => {
+            expect(screen.queryByText(/no sku found/i)).toBeInTheDocument();
+        })
+    });
 })
+
